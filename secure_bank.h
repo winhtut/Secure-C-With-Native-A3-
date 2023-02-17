e8c6a5a41bc3fe96a0028b1190e2bbd2420ff7c5
winhtut <winhtutonline@gmail.com>
2023-02-17 22:38:21 +0630

    email exist checking and char counting

--- secure_bank.h
+++ secure_bank.h
@@ -10,6 +10,13 @@
 void login();
 void rEgister();
 int check_input(char option[2]);
+void gmail_exit_checking(char gmail[50]);
+int char_counting(char my_Char[50]);
+void email_exit_checking(char gmail[50]);
+
+//GlobalVariables
+int G_index=0; // For users
+int email_found=-1;
 
 
 struct trans{
@@ -20,7 +27,7 @@ struct info{
     char name[50];
     char nrc[20];
     char email[50];
-    char password[50];
+    char password[50]; // strong password
     unsigned int phoneNumber;
     char encryption_key[50];//to encrypt user data
     char recovery_key[50];// for recover account
@@ -71,7 +78,11 @@ void login(){
 }
 void rEgister(){
 
+    char rEmail[50];
     printf("This is Swiss Secure Bank Register!\nEnter your gmail>>:");
+    scanf(" %[^\n]",&rEmail[0]);
+    email_found = -1; // get back setting for -1 ( means for negative )
+    email_exit_checking(rEmail);
 }
 int check_input(char option[2]){
     if (option[0] >= 49 && option[1] == '\0' && option[0] <=57) {
@@ -79,5 +90,40 @@ int check_input(char option[2]){
     } else{
         return -1;
     }
+}
+
+void email_exit_checking(char email[50]){
+    int counter = char_counting(email);//for user input email
+    int same_counting=0;
+
+    for(register int gcc=0; gcc<G_index ; gcc++){
+        int email_count =char_counting(db[gcc].email); // for email from db;
+        same_counting=0;
+        if( counter == email_count ){
+            for( register int a=0; a<counter ; a++){
+                if(email[a] != db[gcc].email[a]){
+                    break;
+                }
+                same_counting++;
+            }
+        }
+        if( counter == same_counting){
+            email_found=gcc;
+        }
+    }
+
+
+}
+
+int char_counting(char my_Char[50]){
+    int count=0;
+    for(register int a=0; a<50; a++){
+        if( my_Char[a] =='\0'){
+            break;
+        }
+        count++;
+    }
+
+    return count;
 }
 #endif //NCC_ONLINE_SECURE_BANK_SECURE_BANK_H
